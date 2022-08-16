e05138c0e5cbf5cbad23bd1ae7d8070f535d02cd
vulcan-dev <vitexuhd@gmail.com>
2022-08-16 20:22:54 +0100

    Cleanup

--- SDSaves/SaveManager.cs
+++ SDSaves/SaveManager.cs
@@ -17,169 +17,6 @@ namespace SDSaves {
         private string SavePath;
         private string ActiveSave;
 
-        public void AddSaveToList(string name) {
-            if (!GameSaves.Contains(name)) {
-                GameSaves.Add(name);
-            } else {
-                return; // Overwrite existing save, we don't need to add it again.
-            }
-
-            Button saveNameButton = new Button();
-            saveNameButton.FlatStyle = FlatStyle.Flat;
-            saveNameButton.FlatAppearance.BorderSize = 0;
-            saveNameButton.Text = name;
-            saveNameButton.BackColor = ButtonColor;
-            saveNameButton.ForeColor = Color.White;
-            saveNameButton.Size = ButtonSize;
-            saveNameButton.Name = "Save" + name;
-
-            Button deleteButton = new Button();
-            deleteButton.FlatStyle = FlatStyle.Flat;
-            deleteButton.FlatAppearance.BorderSize = 0;
-            deleteButton.Text = "Delete";
-            deleteButton.BackColor = ButtonColor;
-            deleteButton.ForeColor = Color.White;
-            deleteButton.Size = ButtonSize;
-            deleteButton.Name = "Delete" + name;
-
-            Button overwriteButton = new Button();
-            overwriteButton.FlatStyle = FlatStyle.Flat;
-            overwriteButton.FlatAppearance.BorderSize = 0;
-            overwriteButton.Text = "Save";
-            overwriteButton.BackColor = ButtonColor;
-            overwriteButton.ForeColor = Color.White;
-            overwriteButton.Size = ButtonSize;
-            overwriteButton.Name = "Overwrite" + name;
-
-            deleteButton.Click += DeleteButton_Click;
-            saveNameButton.Click += SaveNameButton_Click;
-            overwriteButton.Click += OverwriteButton_Click;
-
-            ButtonPanel.Controls.Add(saveNameButton);
-            ButtonPanel.Controls.Add(deleteButton);
-            ButtonPanel.Controls.Add(overwriteButton);
-        }
-
-        private string GetHash(string path) {
-            string finalHash = "";
-            string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
-            foreach (string file in files) {
-                byte[] fileBytes = File.ReadAllBytes(file);
-                string fileHash = Sha256.Hash(fileBytes.ToString());
-                finalHash += fileHash;
-            }
-
-            return finalHash;
-        }
-
-        private void SetButtonColor(string name, Color color) {
-            foreach (Control control in ButtonPanel.Controls) {
-                if (control.Name == name) {
-                    control.BackColor = color;
-                } else {
-                    control.BackColor = ButtonColor;
-                }
-            }
-        }
-
-        private string GetCurrentSave() {
-            foreach (string save in GameSaves) {
-                if (GetHash("Saves\\" + save) == GetHash(SavePath)) {
-                    return save;
-                }
-            }
-
-            return "";
-        }
-
-        private void OverwriteButton_Click(object sender, EventArgs e) {
-            Button overwriteButton = (Button)sender;
-            TableLayoutPanel panel = (TableLayoutPanel)overwriteButton.Parent;
-
-            int rowIndex = -1;
-            for (int i = 0; i < panel.Controls.Count; i++) {
-                if (panel.Controls[i].Name == overwriteButton.Name) {
-                    rowIndex = i;
-                    break;
-                }
-            }
-
-            if (rowIndex == -1) {
-                MessageBox.Show("Error finding save!");
-                return;
-            }
-
-            string saveName = panel.Controls[rowIndex-2].Text;
-            if (MessageBox.Show("Are you sure you want to overwrite \"" + saveName + "\"?", "Overwrite Save", MessageBoxButtons.YesNo) == DialogResult.Yes) {
-                string path = $".\\Saves\\{saveName}";
-                if (!Directory.Exists(path)) {
-                    MessageBox.Show("Could not find in \"Saves\" folder!");
-                    return;
-                } else {
-                    ClearDirectory(path);
-                }
-
-                CopyDirectory(SavePath, path);
-            }
-        }
-
-        private void SaveNameButton_Click(object sender, EventArgs e) {
-            Button loadButton = (Button)sender;
-            TableLayoutPanel panel = (TableLayoutPanel)loadButton.Parent;
-
-            int rowIndex = -1;
-            for (int i = 0; i < panel.Controls.Count; i++) {
-                if (panel.Controls[i].Name == loadButton.Name) {
-                    rowIndex = i;
-                    break;
-                }
-            }
-
-            if (rowIndex == -1) {
-                MessageBox.Show("Error finding save!");
-                return;
-            }
-
-            string saveName = panel.Controls[rowIndex].Text;
-            if (MessageBox.Show("Are you sure you want to load \"" + saveName + "\"?", "Load Save", MessageBoxButtons.YesNo) == DialogResult.Yes) {        
-                CopyDirectory(SavePath, ".\\BackupSave\\");
-                ClearDirectory(SavePath);
-                CopyDirectory(".\\Saves\\" + saveName, SavePath);
-
-                SetButtonColor("Save" + saveName, ButtonSaveColor);
-                ActiveSave = saveName;
-            }
-        }
-
-        private void DeleteButton_Click(object sender, EventArgs e) {
-            Button deleteButton = (Button)sender;
-            TableLayoutPanel panel = (TableLayoutPanel)deleteButton.Parent;
-
-            int rowIndex = -1;
-            for (int i = 0; i < panel.Controls.Count; i++) {
-                if (panel.Controls[i].Name == deleteButton.Name) {
-                    rowIndex = i;
-                    break;
-                }
-            }
-            
-            if (rowIndex == -1) {
-                MessageBox.Show("Error finding button!");
-                return;
-            }
-
-            string saveName = panel.Controls[rowIndex-1].Text;
-            if (MessageBox.Show("Are you sure you want to delete \"" + saveName + "\"?", "Delete Save", MessageBoxButtons.YesNo) == DialogResult.Yes) {
-                Directory.Delete(".\\Saves\\" + saveName, true);
-
-                GameSaves.Remove(saveName);
-
-                for (int i = 0; i < 3; i++) {
-                    panel.Controls.RemoveAt(rowIndex-1);
-                }
-            }
-        }
-
         public SaveManager() {
             InitializeComponent();
 
@@ -214,6 +51,90 @@ namespace SDSaves {
             }
         }
 
+        /************************
+        *    Button Utilities   *
+        *************************/
+        private void SetButtonColor(string name, Color color) {
+            foreach (Control control in ButtonPanel.Controls) {
+                if (control.Name == name) {
+                    control.BackColor = color;
+                } else {
+                    control.BackColor = ButtonColor;
+                }
+            }
+        }
+
+        private Button CreateButton(string text, string name) {
+            Button button = new Button();
+            button.FlatStyle = FlatStyle.Flat;
+            button.FlatAppearance.BorderSize = 0;
+            button.Text = text;
+            button.BackColor = ButtonColor;
+            button.ForeColor = Color.White;
+            button.Size = ButtonSize;
+            button.Name = name;
+
+            return button;
+        }
+
+        private Button GetButton(string name, int offset = 0) {
+            for (int i = 0; i < ButtonPanel.Controls.Count; i++) {
+                if (ButtonPanel.Controls[i].Name == name) {
+                    return (Button)ButtonPanel.Controls[i + offset];
+                }
+            }
+
+            return null;
+        }
+
+        /************************
+        *  Helpers & Utilities  *
+        *************************/
+        public void AddSaveToList(string name) {
+            if (!GameSaves.Contains(name)) {
+                GameSaves.Add(name);
+            } else {
+                return; // Overwrite existing save, we don't need to add it again.
+            }
+
+            Button saveNameButton = CreateButton(name, "Save" + name);
+            Button deleteButton = CreateButton("Delete", "Delete" + name);
+            Button overwriteButton = CreateButton("Save", "Overwrite" + name);
+
+            deleteButton.Click += DeleteButton_Click;
+            saveNameButton.Click += SaveNameButton_Click;
+            overwriteButton.Click += OverwriteButton_Click;
+
+            ButtonPanel.Controls.Add(saveNameButton);
+            ButtonPanel.Controls.Add(deleteButton);
+            ButtonPanel.Controls.Add(overwriteButton);
+        }
+
+        private string GetHash(string path) {
+            string finalHash = "";
+            string[] files = Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+            foreach (string file in files) {
+                byte[] fileBytes = File.ReadAllBytes(file);
+                string fileHash = Sha256.Hash(fileBytes.ToString());
+                finalHash += fileHash;
+            }
+
+            return finalHash;
+        }
+
+        private string GetCurrentSave() {
+            foreach (string save in GameSaves) {
+                if (GetHash("Saves\\" + save) == GetHash(SavePath)) {
+                    return save;
+                }
+            }
+
+            return "";
+        }
+
+        /****************************
+         * File/Directory Utilities *
+        *****************************/
         void CopyDirectory(string src, string dst, bool recurse = true, bool overwrite = true) {
             DirectoryInfo source = new DirectoryInfo(src);
             DirectoryInfo destination = new DirectoryInfo(dst);
@@ -251,7 +172,57 @@ namespace SDSaves {
             }
         }
 
-        private void createSaveButton_Click(object sender, EventArgs e) {
+        
+        /************************
+         * Handle Button Clicks *
+        *************************/
+        private void OverwriteButton_Click(object sender, EventArgs e) { // Save/Overwrite Button
+            Button overwriteButton = (Button)sender;
+
+            string saveName = GetButton(overwriteButton.Name, -2).Text;
+            if (MessageBox.Show("Are you sure you want to overwrite \"" + saveName + "\"?", "Overwrite Save", MessageBoxButtons.YesNo) == DialogResult.Yes) {
+                string path = $".\\Saves\\{saveName}";
+                if (!Directory.Exists(path)) {
+                    MessageBox.Show("Could not find in \"Saves\" folder!");
+                    return;
+                } else {
+                    ClearDirectory(path);
+                }
+
+                CopyDirectory(SavePath, path);
+            }
+        }
+
+        private void SaveNameButton_Click(object sender, EventArgs e) { // Load Button
+            Button loadButton = (Button)sender;
+
+            string saveName = GetButton(loadButton.Name).Text;
+            if (MessageBox.Show("Are you sure you want to load \"" + saveName + "\"?", "Load Save", MessageBoxButtons.YesNo) == DialogResult.Yes) {        
+                CopyDirectory(SavePath, ".\\BackupSave\\");
+                ClearDirectory(SavePath);
+                CopyDirectory(".\\Saves\\" + saveName, SavePath);
+
+                SetButtonColor("Save" + saveName, ButtonSaveColor);
+                ActiveSave = saveName;
+            }
+        }
+
+        private void DeleteButton_Click(object sender, EventArgs e) { // Delete Button
+            Button deleteButton = (Button)sender;
+
+            string saveName = GetButton(deleteButton.Name, -1).Text;
+            if (MessageBox.Show("Are you sure you want to delete \"" + saveName + "\"?", "Delete Save", MessageBoxButtons.YesNo) == DialogResult.Yes) {
+                Directory.Delete(".\\Saves\\" + saveName, true);
+
+                GameSaves.Remove(saveName);
+
+                for (int i = 0; i < 3; i++) {
+                    ButtonPanel.Controls.RemoveAt(ButtonPanel.Controls.Count - 1);
+                }
+            }
+        }
+
+        private void createSaveButton_Click(object sender, EventArgs e) { // Create Save Button
             string saveName = Controls.Find("input_saveName", true).FirstOrDefault().Text;
             if (saveName == "") {
                 MessageBox.Show("Please enter a name for the save.");
